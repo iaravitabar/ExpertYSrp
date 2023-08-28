37f3ff87aba9c0e446ce9b07b3165b00cfd97550
iaravitabar <141652292+iaravitabar@users.noreply.github.com>
2023-08-28 12:12:27 -0300

    Update Program.cs
    sol

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,6 +9,8 @@ namespace SRP
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
+            //Library library1 = new Library("A","7");//
+            //Library library2 = new Library("B","3")//
             book1.ShelveBook("A","7");
             book2.ShelveBook("B","3");
 
